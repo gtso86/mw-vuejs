b1b2ecdb9b5bb8465f4103387aba665b260752bb
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-01 23:06:04 -0300

    change import/prefer-default-export rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     '@vue/prettier',
   ],
   rules: {
+    'import/prefer-default-export': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
